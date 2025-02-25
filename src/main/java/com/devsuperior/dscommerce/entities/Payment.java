8abfe92fcf771445a264d2cb3775c465dc6387f4
Andre Augusto Laudares Ansani <andre.ansani@innomotics.com>
2025-02-25 17:45:00 -0300

    Payment

---
+++ src/main/java/com/devsuperior/dscommerce/entities/Payment.java
@@ -0,0 +1,67 @@
+package com.devsuperior.dscommerce.entities;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment(){
+    }
+
+    public Payment(long id, Instant moment, Order order) {
+        Id = id;
+        this.moment = moment;
+        this.order = order;
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment [Id=" + Id + ", moment=" + moment + ", order=" + order + "]";
+    }
+
+    
+}
