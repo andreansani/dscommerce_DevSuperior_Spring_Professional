53e60d8b8c471d1f8d3d37330e89b0445ef841f5
Andre Ansani <andre.ansani@innomotics.com>
2026-02-01 11:50:52 -0300

    Get Logged User

---
+++ src/main/java/com/devsuperior/dscommerce/dto/UserDTO.java
@@ -0,0 +1,88 @@
+package com.devsuperior.dscommerce.dto;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import com.devsuperior.dscommerce.entities.User;
+
+public class UserDTO {
+
+    private long id;
+    
+    private String name;
+   
+    private String email;
+    
+    private String phone;
+    
+    private LocalDate birthDate;
+
+    private List<String> roles = new ArrayList<>();
+
+    public UserDTO() {
+    }
+
+    public UserDTO(User entity) {
+        id = entity.getId();
+        name = entity.getName();
+        email = entity.getEmail();
+        phone = entity.getPhone();
+        birthDate = entity.getBirthDate();
+
+        for (GrantedAuthority role: entity.getRoles()){
+            roles.add(role.getAuthority());
+        }
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+    
+}
